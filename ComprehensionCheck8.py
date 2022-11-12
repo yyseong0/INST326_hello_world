6e050b7065cb06fbffb96537a3e90db81bc0373c
Ellen Seong <115054947+yyseong0@users.noreply.github.com>
2022-11-11 22:29:32 -0500

    Create ComprehensionCheck8.py

---
+++ ComprehensionCheck8.py
@@ -0,0 +1,7 @@
+echo "# INST326_hello_world" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yyseong0/INST326_hello_world.git
+git push -u origin main
