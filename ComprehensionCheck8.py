038e235106caef04e4560b4a36e0c0b6c00e4922
Ellen Seong <115054947+yyseong0@users.noreply.github.com>
2022-11-11 22:34:05 -0500

    Update ComprehensionCheck8.py

--- ComprehensionCheck8.py
+++ ComprehensionCheck8.py
@@ -5,3 +5,7 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/yyseong0/INST326_hello_world.git
 git push -u origin main
+
+git remote add origin https://github.com/yyseong0/INST326_hello_world.git
+git branch -M main
+git push -u origin main
